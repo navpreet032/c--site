dfe4f7e36fa38dfa29819be7a5000973ffdd550e
navpannu032 <preetpannu032@gmail.com>
2021-03-19 15:02:25 +0530

    cgnh

---
+++ script1.js
@@ -0,0 +1,175 @@
+
+
+function chBgColo0(element) {
+    var bg = document.getElementById("list1 tab0");
+
+    bg.style.backgroundColor = "rgb(25, 132, 231)";
+    document.getElementById("list1 tab1").style.backgroundColor = "white";
+    document.getElementById("list1 tab2").style.backgroundColor = "white";
+    document.getElementById("list1 tab3").style.backgroundColor = "white";
+    document.getElementById("list1 tab4").style.backgroundColor = "white";
+
+   
+    var x = document.getElementById('collapsetxt0');//tab1 text 
+    x.style.display ='none';
+    if (x.style.display == 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+
+    if (document.getElementById('collapsetxt1').style.display == 'block') { //to close tab1 text
+        document.getElementById('collapsetxt1').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt2').style.display == 'block') { //to close tab1 text
+        document.getElementById('collapsetxt2').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt3').style.display == 'block') { //to close tab1 text
+        document.getElementById('collapsetxt3').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt4').style.display == 'block') { //to close tab1 text
+        document.getElementById('collapsetxt4').style.display = 'none';
+    } 
+    
+
+
+}
+
+
+
+
+function chBgColo1(element) {
+    var bg = document.getElementById("list1 tab1");
+
+    bg.style.backgroundColor = "rgb(25, 132, 231)";
+    document.getElementById("list1 tab2").style.backgroundColor = "white";
+    document.getElementById("list1 tab3").style.backgroundColor = "white";
+    document.getElementById("list1 tab4").style.backgroundColor = "white";
+    document.getElementById("list1 tab0").style.backgroundColor = "white";
+
+    var x = document.getElementById('collapsetxt1');//tab1 text 
+    if (x.style.display == 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+
+    
+    if (document.getElementById('collapsetxt0').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt0').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt2').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt2').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt3').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt3').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt4').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt4').style.display = 'none';
+    } 
+
+}
+
+function chBgColo2(element) {
+    var bg = document.getElementById("list1 tab2");
+
+    bg.style.backgroundColor = "rgb(25, 132, 231)";
+    document.getElementById("list1 tab1").style.backgroundColor = "white";
+    document.getElementById("list1 tab3").style.backgroundColor = "white";
+    document.getElementById("list1 tab4").style.backgroundColor = "white";
+    document.getElementById("list1 tab0").style.backgroundColor = "white";
+
+    var x = document.getElementById('collapsetxt2');//tab1 text 
+    if (x.style.display == 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+
+    if (document.getElementById('collapsetxt1').style.display == 'block') { //to close ta text
+        document.getElementById('collapsetxt1').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt0').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt0').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt3').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt3').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt4').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt4').style.display = 'none';
+    } 
+
+
+}
+
+
+
+function chBgColo3(element) {
+    var bg = document.getElementById("list1 tab3");
+
+    bg.style.backgroundColor = "rgb(25, 132, 231)";
+    document.getElementById("list1 tab2").style.backgroundColor = "white";
+    document.getElementById("list1 tab4").style.backgroundColor = "white";
+    document.getElementById("list1 tab1").style.backgroundColor = "white";
+    document.getElementById("list1 tab0").style.backgroundColor = "white";
+
+    var x = document.getElementById('collapsetxt3');//tab1 text 
+    if (x.style.display == 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+    
+    if (document.getElementById('collapsetxt1').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt1').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt2').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt2').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt0').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt0').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt4').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt4').style.display = 'none';
+    } 
+
+}
+
+
+
+function chBgColo4(element) {
+    var bg = document.getElementById("list1 tab4");
+
+    bg.style.backgroundColor = "rgb(25, 132, 231)";
+    document.getElementById("list1 tab0").style.backgroundColor = "white";
+    document.getElementById("list1 tab2").style.backgroundColor = "white";
+    document.getElementById("list1 tab3").style.backgroundColor = "white";
+    document.getElementById("list1 tab1").style.backgroundColor = "white";
+
+    var x = document.getElementById('collapsetxt4');//tab1 text 
+    if (x.style.display == 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+
+    if (document.getElementById('collapsetxt1').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt1').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt2').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt2').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt3').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt3').style.display = 'none';
+    } 
+    if (document.getElementById('collapsetxt0').style.display == 'block') { //to close tab text
+        document.getElementById('collapsetxt0').style.display = 'none';
+    } 
+
+}
+
